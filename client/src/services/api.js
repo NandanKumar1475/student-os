71be0d79fb5aff439e37786f81f17606f85ced1f
Nandan <nandankumar1475@gmail.com>
2026-05-18 00:39:28 +0530

    refactor: simplify api.js by importing axios instance directly

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,23 +1,5 @@
 // client/src/services/api.js
 
-import axios from "axios";
-import { API_BASE_URL } from "../config/api";
-
-const api = axios.create({
-    baseURL: API_BASE_URL,
-    headers: {
-        "Content-Type": "application/json"
-    }
-});
-
-api.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
-
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-});
+import api from '../api/axiosInstance';
 
 export default api;
